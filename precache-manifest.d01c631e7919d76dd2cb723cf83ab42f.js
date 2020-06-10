324f68f0bbb813017add3b680746a98f4d114dea
OlegSuncrown <olegsuncrown@gmail.com>
2020-06-10 10:56:20 +0300

    Updates

---
+++ precache-manifest.d01c631e7919d76dd2cb723cf83ab42f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43a7b4e494087ddaf6ba94ce1fa5cf5",
+    "url": "/netflix_clone_responsive/index.html"
+  },
+  {
+    "revision": "88248f545aeb1cc1439e",
+    "url": "/netflix_clone_responsive/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ee0d9bb641e34dec6948",
+    "url": "/netflix_clone_responsive/static/css/main.e82c1514.chunk.css"
+  },
+  {
+    "revision": "88248f545aeb1cc1439e",
+    "url": "/netflix_clone_responsive/static/js/2.58440291.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/netflix_clone_responsive/static/js/2.58440291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0d9bb641e34dec6948",
+    "url": "/netflix_clone_responsive/static/js/main.076c177c.chunk.js"
+  },
+  {
+    "revision": "81f297b0ede370c331bf",
+    "url": "/netflix_clone_responsive/static/js/runtime-main.d38947dc.js"
+  },
+  {
+    "revision": "8e8f998a41c0dc1b813657d307f98c93",
+    "url": "/netflix_clone_responsive/static/media/card1.8e8f998a.png"
+  },
+  {
+    "revision": "587e040f20259792094901ca9739669e",
+    "url": "/netflix_clone_responsive/static/media/card2-icon.587e040f.png"
+  },
+  {
+    "revision": "a48333d43c19612ed61987fa5dbff3c5",
+    "url": "/netflix_clone_responsive/static/media/card2.a48333d4.jpg"
+  },
+  {
+    "revision": "ee67ec03e849d04360d16709e06597b8",
+    "url": "/netflix_clone_responsive/static/media/card3.ee67ec03.png"
+  },
+  {
+    "revision": "eecc293bf13bf792c98bf4830d383fe6",
+    "url": "/netflix_clone_responsive/static/media/header.eecc293b.jpg"
+  },
+  {
+    "revision": "de24d3a289167a8cb227f6b3ad35dd4b",
+    "url": "/netflix_clone_responsive/static/media/logo.de24d3a2.svg"
+  }
+]);
